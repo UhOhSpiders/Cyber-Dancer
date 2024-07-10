59a55b331282ec71f985c7c9675f2ce0646be478
Iain <iain.mccall.95@gmail.com>
2024-07-10 21:18:46 +0100

    fixed target scale breakpoint bug and added score resize

--- front-end/src/Score.js
+++ front-end/src/Score.js
@@ -1,4 +1,5 @@
 import { Text } from "troika-three-text";
+import { sceneLayoutWidthBreakpoint } from "./constants/constants";
 
 export default class Score {
   constructor(scene, cameraPosition) {
@@ -10,12 +11,13 @@ export default class Score {
   }
   createDisplay() {
     this.text.text = this.scoreDetails.total;
-    this.text.position.x = this.cameraPosition.x + 0.17;
     this.text.position.y = this.cameraPosition.y + 0.15;
     this.text.position.z = this.cameraPosition.z - 0.4
     this.text.fontSize = 0.04
+    this.text.anchorX= 'right'
     this.text.sync();
     this.scene.add(this.text);
+    console.log(this.text)
   }
   increase() {
     this.currentStreak += 1
@@ -37,5 +39,16 @@ export default class Score {
   getScoreDetails(){
     this.scoreDetails.allStreaks.push(this.currentStreak)
     return this.scoreDetails
+  }
+
+  setSize(width){
+    if(width < sceneLayoutWidthBreakpoint){
+      this.text.position.x = this.cameraPosition.x + 0.065;
+      this.text.fontSize = 0.02
+      this.text.sync();
+    }else{
+      this.text.position.x = this.cameraPosition.x + 0.25;
+      this.text.fontSize = 0.03
+    }
   }
 }
